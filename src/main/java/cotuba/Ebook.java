ed4ece7dd110f0104d00f7a52c1a1b13bc924336
Gabriel Ronei <gabriheron@hotmail.com>
2020-04-01 17:07:14 -0300

    [Refatorando] Utilizando o SRP para retirar a responsabilidade multipla do main

---
+++ src/main/java/cotuba/Ebook.java
@@ -0,0 +1,42 @@
+package cotuba;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class Ebook {
+
+	private String formato;
+	private Path arquivoDeSaida;
+	private List<Capitulo> capitulos;
+
+	public Ebook(String formato, Path arquivoDeSaida, List<Capitulo> capitulos) {
+		super();
+		this.formato = formato;
+		this.arquivoDeSaida = arquivoDeSaida;
+		this.capitulos = capitulos;
+	}
+
+	public String getFormato() {
+		return formato;
+	}
+
+	public void setFormato(String formato) {
+		this.formato = formato;
+	}
+
+	public Path getArquivoDeSaida() {
+		return arquivoDeSaida;
+	}
+
+	public void setArquivoDeSaida(Path arquivoDeSaida) {
+		this.arquivoDeSaida = arquivoDeSaida;
+	}
+
+	public List<Capitulo> getCapitulos() {
+		return capitulos;
+	}
+
+	public void setCapitulos(List<Capitulo> capitulos) {
+		this.capitulos = capitulos;
+	}
+}
---
+++ src/main/java/cotuba/GeradorEPUB.java
@@ -0,0 +1,33 @@
+package cotuba;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Resource;
+import nl.siegmann.epublib.epub.EpubWriter;
+import nl.siegmann.epublib.service.MediatypeService;
+
+public class GeradorEPUB {
+
+	public void gera(Ebook ebook) {
+		Path arquivoDeSaida = ebook.getArquivoDeSaida();
+		Book epub = new Book();
+
+		for (Capitulo capitulo : ebook.getCapitulos()) {
+
+			epub.addSection(capitulo.getTitulo(),
+					new Resource(capitulo.getConteudoHtml().getBytes(), MediatypeService.XHTML));
+		}
+
+		EpubWriter epubWriter = new EpubWriter();
+
+		try {
+			epubWriter.write(epub, Files.newOutputStream(arquivoDeSaida));
+		} catch (IOException ex) {
+			throw new RuntimeException("Erro ao criar arquivo EPUB: " + arquivoDeSaida.toAbsolutePath(), ex);
+		}
+	}
+
+}
